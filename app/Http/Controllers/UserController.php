82c9e3d8e0e4753aa166dc0a378533867a3adef0
Derrick Mulinde <djmulinde@gmail.com>
2023-06-21 21:22:20 +0300

    Add avatar route files
    - Create routes to view the form in which the user manages their avatars.
    
    - The uploaded avatar should be validated (Image, less than 5MB)
    
    - If a user already has an avatar, just replace their existing avatar, both in the table and in the storage folder

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
@@ -69,5 +71,49 @@ class UserController extends Controller
         // Username - Find user depending on the username(its unique)
 
         return view('user-profile', ['username' => $user->username, 'posts' => $user->posts()->latest()->get(), 'postCount' => $user->posts()->count()]);
+    }
+
+    /*
+        * Manage Avatars
+    */
+
+    public function showManageAvatarForm()
+    {
+        return view('manage-avatar');
+    }
+
+    public function uploadAvatar(Request $request)
+    {
+        // Validate the avatar file
+        $request->validate([
+            'avatar' => 'required|max:3000|image'
+        ]);
+
+        // Use the intervention/image library to set dimensions of the avatar
+        // Give it 120x120 pixels dimensions
+        // Encode it into a jpg
+        $avatarData = Image::make($request->file('avatar'))->fit(120)->encode('jpg');
+
+        // Create a unique name for the avatar
+        // Username name and a unique string
+        $user = auth()->user();
+        $avatarName = $user->username . '-' . uniqid() . '.jpg';
+
+        // Store the enoded avatar image data in the public folder
+        Storage::put('public/avatars/' . $avatarName, $avatarData);
+
+        $oldAvatar = $user->avatar;
+
+        // Saving the avatar into the database
+        $user->avatar = $avatarName;
+        $user->save();
+
+        // string(42) "/storage/avatars/derrick-6492cf2c6209e.jpg"
+        // Replace /storage/ with /public/ and then delete photo
+        if ($oldAvatar != "/default-avatar.jpg") {
+            Storage::delete(str_replace("/storage/", "public/", $oldAvatar));
+        }
+
+        return back()->with('success', 'Avatar uploaded successfully!');
     }
 }
